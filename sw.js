4e774639eb9a38c201a8672f231ab0cc1b85af44
hissa <chourib.akrem@gmail.com>
2026-09-13 13:16:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='gk-v2';
+const ASSETS=['./','./index.html','./manifest.json',
+  './icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>{}));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request)).catch(()=>caches.match('./index.html')));
+});
